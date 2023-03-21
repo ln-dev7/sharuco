c823a38426c599756e82a2fd59743778fa4f61b8
LN <62269693+ln-dev7@users.noreply.github.com>
2023-03-21 11:28:10 +0100

    update

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,12 +1,17 @@
+"use client"
+
 import Head from "next/head"
 import Link from "next/link"
+import { useToast } from "@/hooks/use-toast"
 import { Github, Loader2 } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
 import { Layout } from "@/components/layout"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { ToastAction } from "@/components/ui/toast"
 
 export default function Explore() {
+  const { toast } = useToast()
   return (
     <Layout>
       <Head>
@@ -24,6 +29,22 @@ export default function Explore() {
           <h1 className="text-2xl font-extrabold leading-tight tracking-tighter sm:text-2xl md:text-4xl lg:text-4xl">
             Discover little bits of code that can help you.
           </h1>
+          <Button
+            variant="outline"
+            onClick={() => {
+              toast({
+                title: "Scheduled: Catch up ",
+                description: "Friday, February 10, 2023 at 5:57 PM",
+                action: (
+                  <ToastAction altText="Goto schedule to undo">
+                    Undo
+                  </ToastAction>
+                ),
+              })
+            }}
+          >
+            Add to calendar
+          </Button>
         </div>
       </section>
     </Layout>
