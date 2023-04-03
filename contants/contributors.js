4bf0f9d7ccf615bd616182f26f35699f2bd62129
LN <62269693+ln-dev7@users.noreply.github.com>
2023-04-03 17:24:41 +0100

    add contributors

---
+++ contants/contributors.js
@@ -0,0 +1,17 @@
+export const contributors = [
+  {
+    name: "LN",
+    github: "https://github.com/ln_dev7",
+    image: "https://avatars.githubusercontent.com/u/62269693?v=4",
+  },
+  {
+    name: "Sanix-Darker",
+    github: "https://github.com/Sanix-Darker",
+    image: "https://avatars.githubusercontent.com/u/22576758?v=4",
+  },
+  {
+    name: "mckenziearts",
+    github: "https://github.com/mckenziearts",
+    image: "https://avatars.githubusercontent.com/u/14105989?v=4",
+  },
+]
