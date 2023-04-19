482c79ee39bbf12b9049fd123b73648bd364fa05
ln-dev7 <leonelngoya@gmail.com>
2023-04-19 19:41:51 +0100

    implement search bar on header

---
+++ components/search-bar.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { DialogProps } from "@radix-ui/react-alert-dialog"
+import algoliasearch from "algoliasearch"
+import {
+  Circle,
+  File,
+  Laptop,
+  Moon,
+  Search,
+  SearchIcon,
+  SunMedium,
+  Trash2,
+} from "lucide-react"
+import { useTheme } from "next-themes"
+import {
+  Highlight,
+  Hits,
+  InstantSearch,
+  SearchBox,
+} from "react-instantsearch-hooks-web"
+
+import { cn } from "@/lib/utils"
+import Loader from "@/components/loader"
+import LoaderCode from "@/components/loader-code"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import AlgoliaSearch from "./algolia-search"
+
+export function SearchBar({ ...props }: DialogProps) {
+  const [open, setOpen] = React.useState(false)
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button
+          defaultChecked={false}
+            variant="outline"
+            className={cn(
+              "relative h-9 w-full justify-start rounded-[0.5rem] text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-96"
+            )}
+            onClick={() => setOpen(true)}
+            {...props}
+          >
+            <span className="hidden lg:inline-flex">
+              Search publics codes ...
+            </span>
+            <span className="inline-flex lg:hidden">Search...</span>
+            <kbd className="pointer-events-none absolute right-1.5 top-2 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+              <span className="text-xs">⌘</span>K
+            </kbd>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-h-[640px] p-0 overflow-hidden overflow-y-auto scrollbar-hide">
+          <AlgoliaSearch />
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,14 +9,20 @@ import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { buttonVariants } from "@/components/ui/button"
 import { AvatarUser } from "./avatar-user"
+import { SearchBar } from "./search-bar"
 
 export function SiteHeader() {
   const { user } = useAuthContext()
   return (
     <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
+        <div>
+          <MainNav items={siteConfig.mainNav} />
+        </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
+            <SearchBar />
+          </div>
           <nav className="flex items-center space-x-1">
             <Link
               href={siteConfig.links.github}
