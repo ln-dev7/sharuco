4feb5e81ecba6eab05316fba0ed90822740e5e73
ln-dev7 <leonelngoya@gmail.com>
2023-04-21 00:55:00 +0100

    init donation page

---
+++ notchpay/donation/initializePayment.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import axios from "axios"
+
+const NEXT_PUBLIC_NOTCH_PAY_PUBLIC_KEY =
+  process.env.NEXT_PUBLIC_NOTCH_PAY_PUBLIC_KEY
+
+const NEXT_PUBLIC_NOTCH_PAY_API_URL = process.env.NEXT_PUBLIC_NOTCH_PAY_API_URL
+
+const INITIALIZE_URL = `${NEXT_PUBLIC_NOTCH_PAY_API_URL}/initialize`
+
+export default function usePaymentInitialization() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const initializePayment = async (email, amount, description) => {
+    setIsLoading(true)
+    setIsError(false)
+
+    const data = {
+      email,
+      currency: "EUR",
+      amount,
+      description,
+      callback: "http://localhost:3000/donation",
+      //callback: "https://sharuco.lndev.me/donation",
+    }
+
+    try {
+      const response = await axios.post(INITIALIZE_URL, data, {
+        headers: {
+          Accept: "application/json",
+          Authorization: NEXT_PUBLIC_NOTCH_PAY_PUBLIC_KEY,
+        },
+      })
+
+      const authorizationUrl = response.data.authorization_url
+      // console.log(response.data)
+      // stock la reference dans le local storage
+      localStorage.setItem(
+        "transaction-reference",
+        response.data.transaction.reference
+      )
+      window.open(authorizationUrl, "_blank")
+    } catch (error) {
+      setIsError(true)
+      // console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { initializePayment, isLoading, isError }
+}
