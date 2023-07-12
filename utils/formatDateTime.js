83af7e941acc3159047c67fbedd5916ab5283f15
LN <leonelngoya@gmail.com>
2023-07-12 12:23:39 +0100

    feat: add formatDateTime

---
+++ utils/formatDateTime.js
@@ -0,0 +1,20 @@
+import moment from 'moment';
+import 'moment/locale/fr'; 
+
+function formatDateTime(date) {
+  moment.locale('en');
+  const now = moment();
+  const targetDateUtc = moment.utc(date, 'YYYY-MM-DD HH:mm:ss');
+  const targetDateLocal = targetDateUtc.local();
+
+  // conast targetDateLocal = moment(date);
+
+  if (now.diff(targetDateLocal, 'days') >= 7) {
+    return targetDateLocal.format('DD MMM YYYY');
+  } else {
+    return targetDateLocal.fromNow();
+  }
+  
+}
+
+export default formatDateTime;
