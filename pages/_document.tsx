3fcf77e741b9ff7309227a5a2681cdb4ec6a50d7
LN <leonelngoya@gmail.com>
2023-11-16 13:26:04 +0100

    google ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-adsense-account" content="ca-pub-2222017759396595" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2222017759396595"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body className="min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
         <Main />
